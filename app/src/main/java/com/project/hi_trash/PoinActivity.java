f0c11c956f575ff2046a4b2c5d79f2e11f838ac3
putrirachmawati <58167900+putrirachmawati@users.noreply.github.com>
2020-05-27 08:28:23 +0700

    UI Riwayat poin

---
+++ app/src/main/java/com/project/hi_trash/PoinActivity.java
@@ -0,0 +1,18 @@
+package com.project.hi_trash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class PoinActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poin);
+    }
+}

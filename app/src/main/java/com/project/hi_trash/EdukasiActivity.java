f4f46058d4e0a98683fa48ac2143cd6225bb19fd
aidatulfakhirah <aidatulfakhirah@gmail.com>
2020-06-09 20:35:22 +0700

    UI Edukasi

---
+++ app/src/main/java/com/project/hi_trash/EdukasiActivity.java
@@ -0,0 +1,14 @@
+package com.project.hi_trash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EdukasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edukasi);
+    }
+}

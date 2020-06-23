c333702164618562d8c9c2e72b9de86b2586b079
Septiyan333 <Septiyan333@gmail.com>
2020-06-23 23:15:40 +0700

    API

---
+++ app/src/main/java/com/project/hi_trash/Url.java
@@ -0,0 +1,8 @@
+package com.project.hi_trash;
+
+public class Url {
+        //    api endpoint untuk request volley
+        public static final String BASE = "https://hitrash.mif-project.com/";
+        public static final String API_LOGIN = BASE+"Api_login/masuk";
+        public static final String API_REGISTER = BASE+"Api_login/register";
+}

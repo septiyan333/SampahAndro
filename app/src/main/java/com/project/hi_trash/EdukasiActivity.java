851484c8425460226aabfa1999f2ea5da601e005
putrirachmawati <58167900+putrirachmawati@users.noreply.github.com>
2020-06-27 00:20:54 -0700

    Intent

--- app/src/main/java/com/project/hi_trash/EdukasiActivity.java
+++ app/src/main/java/com/project/hi_trash/EdukasiActivity.java
@@ -1,14 +1,32 @@
 package com.project.hi_trash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EdukasiActivity extends AppCompatActivity {
+    ImageButton imgbtnorganik, imgbtnanorganik;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edukasi);
+        imgbtnorganik = (ImageButton) findViewById(R.id.imgbtnorganik);
+        imgbtnanorganik = (ImageButton) findViewById(R.id.imgbtnanorganik);
+
+        imgbtnorganik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SampahAnorganik.class));
+            }
+        });imgbtnanorganik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SampahOrganik.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/project/hi_trash/HomeActivity.java
+++ app/src/main/java/com/project/hi_trash/HomeActivity.java
@@ -1,13 +1,61 @@
 package com.project.hi_trash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    Button jemput, riwayat, jadwal, poin, edukasi, profil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        jemput = (Button) findViewById(R.id.jemput);
+        riwayat = (Button) findViewById(R.id.riwayat);
+        jadwal = (Button) findViewById(R.id.jadwal);
+        poin = (Button) findViewById(R.id.poin);
+        edukasi = (Button) findViewById(R.id.edukasi);
+        profil = (Button) findViewById(R.id.profil);
+
+        jemput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),PenjemputanActivity.class));
+            }
+        });
+        riwayat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),RiwayatActivity.class));
+            }
+        });
+        jadwal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),JadwalActivity.class));
+            }
+        });
+        poin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),PoinActivity.class));
+            }
+        });
+        edukasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),EdukasiActivity.class));
+            }
+        });
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),ProfilActivity.class));
+            }
+        });
     }
 }

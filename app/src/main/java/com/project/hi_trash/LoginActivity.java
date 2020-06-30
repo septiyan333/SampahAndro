9627b9d3519df77920f49df21b61c62528a78952
putrirachmawati <58167900+putrirachmawati@users.noreply.github.com>
2020-06-30 07:39:26 -0700

    repair penjemputan

--- app/src/main/java/com/project/hi_trash/LoginActivity.java
+++ app/src/main/java/com/project/hi_trash/LoginActivity.java
@@ -1,32 +1,11 @@
 package com.project.hi_trash;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.textfield.TextInputLayout;
-import com.project.hi_trash.SessionManager.SessionManager;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     Button masuk,buat;
